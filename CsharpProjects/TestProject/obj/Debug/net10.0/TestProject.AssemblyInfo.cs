0990d8551caa3e154c2bf6fcd3c5922f4a705d19
Joy-Zwarts <25042939@student.hhs.nl>
2026-09-07 13:53:58 +0200

    Portfolio klaar

--- CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e35e31c43b47894fbdcd881957a00a447a61f2af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ab696912da36e8553bbc25c2a4bebbd2a8c502")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

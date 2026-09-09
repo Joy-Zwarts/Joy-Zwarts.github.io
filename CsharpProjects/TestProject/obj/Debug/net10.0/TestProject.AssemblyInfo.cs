06d871612ddd58d8540efa2ef664c871abc865c0
Joy-Zwarts <25042939@student.hhs.nl>
2026-09-09 10:48:34 +0200

    Kijken of dit werkt

--- CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ab696912da36e8553bbc25c2a4bebbd2a8c502")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca188a405adef77893ab1a7da54240a4aef4f835")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
